2ad4462d5cee1af1b7b32f6014085da0fe65d826
EzekielDaun <liuyifei0905@gmail.com>
2023-11-24 11:02:59 -0800

    init🎉: First Commit

---
+++ part2.py
@@ -0,0 +1,21 @@
+# Group #: G35
+# Student Names: Yifei Liu, Liying Xian
+
+import threading
+import queue
+import time, random
+
+
+def consumerWorker(queue):
+    """target worker for a consumer thread"""
+    # to implement
+
+
+def producerWorker(queue):
+    """target worker for a producer thread"""
+    # to implement
+
+
+if __name__ == "__main__":
+    buffer = queue.Queue()
+    # to implement
