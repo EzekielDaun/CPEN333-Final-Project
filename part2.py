72bb0525545296e4c5156bf7b1dfd283a44e8475
EzekielDaun <liuyifei0905@gmail.com>
2023-11-24 12:40:04 -0800

    feat✨: part 2

--- part2.py
+++ part2.py
@@ -6,16 +6,54 @@ import queue
 import time, random
 
 
-def consumerWorker(queue):
+def consumerWorker(queue: queue.Queue):
     """target worker for a consumer thread"""
-    # to implement
+
+    while True:
+        item = queue.get()
+        print(f"Item consumed: {item}")
+        queue.task_done()
+        time.sleep(round(random.uniform(0.1, 0.3), 2))  # wait for 100-300ms
 
 
-def producerWorker(queue):
+def producerWorker(queue: queue.Queue):
     """target worker for a producer thread"""
-    # to implement
+
+    number_of_items = 5  # items per producer thread produces
+    for _ in range(number_of_items):
+        item = random.randint(1, 100)  # generate a random integer between 1 and 100
+        queue.put(item)
+        print(f"Item produced: {item}")
+        time.sleep(round(random.uniform(0.1, 0.3), 2))  # wait for 100-300ms
 
 
 if __name__ == "__main__":
     buffer = queue.Queue()
-    # to implement
+
+    # 4 producer threads
+    producers = [
+        threading.Thread(target=producerWorker, args=(buffer,)) for _ in range(4)
+    ]
+
+    # 5 consumer threads
+    consumers = [
+        threading.Thread(target=consumerWorker, args=(buffer,), daemon=True)
+        for _ in range(5)
+    ]
+
+    # start all producers
+    for p in producers:
+        p.start()
+
+    # start all consumers
+    for c in consumers:
+        c.start()
+
+    # wait for all producers to finish
+    for p in producers:
+        p.join()
+
+    # wait for all daemon consumers to "finish" (consume all items in the buffer queue)
+    buffer.join()
+
+    print("All Done!")
