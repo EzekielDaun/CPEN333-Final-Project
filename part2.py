1f6ab65beba0dd18e0708bd7d1bed563cf82781d
EzekielDaun <liuyifei0905@gmail.com>
2023-11-24 21:48:21 -0800

    docs📝: part 2 add comment

--- part2.py
+++ part2.py
@@ -10,21 +10,27 @@ def consumerWorker(queue: queue.Queue):
     """target worker for a consumer thread"""
 
     while True:
+        # wait for an item from the queue
         item = queue.get()
         print(f"Item consumed: {item}")
+        # signal that the task is done
         queue.task_done()
-        time.sleep(round(random.uniform(0.1, 0.3), 2))  # wait for 100-300ms
+        # wait for 100-300ms
+        time.sleep(round(random.uniform(0.1, 0.3), 2))
 
 
 def producerWorker(queue: queue.Queue):
     """target worker for a producer thread"""
 
-    number_of_items = 5  # items per producer thread produces
+    number_of_items = 3  # items per producer thread produces
     for _ in range(number_of_items):
-        item = random.randint(1, 100)  # generate a random integer between 1 and 100
+        # generate a random integer between 1 and 100
+        item = random.randint(1, 100)
+        # put the item in the queue
         queue.put(item)
         print(f"Item produced: {item}")
-        time.sleep(round(random.uniform(0.1, 0.3), 2))  # wait for 100-300ms
+        # wait for 100-300ms
+        time.sleep(round(random.uniform(0.1, 0.3), 2))
 
 
 if __name__ == "__main__":
